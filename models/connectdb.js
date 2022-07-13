a590e5b08e27dfd1ae4eade7a36f01178294526a
mmengi18 <mmengi18@montgomerycollege.edu>
2022-07-13 16:21:19 -0400

    commited for issue que request

---
+++ models/connectdb.js
@@ -0,0 +1,20 @@
+// require dotenv to load .env file and get enviromental variable DATABASE_URI 
+const dotenv = require('dotenv').config()
+// run mongoose
+const mongoose = require('mongoose')
+
+// connect mongoose to mongodb database/room
+mongoose.connect(process.env.DATABASE_URI, 
+    { useNewUrlParser: true,
+    useUnifiedTopology: true
+     })
+
+    //  check weather you are succesfully connected to mongodb database
+const db = mongoose.connection
+db.once('open', _ => {
+  console.log('Database connected:', process.env.DATABASE_URI)
+})
+
+db.on('error', err => {
+  console.error('connection error:', err)
+})
